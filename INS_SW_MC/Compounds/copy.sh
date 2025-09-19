c7da77e69226c1cd9ec3fd182866669cc1593dd2
malaei <mojtaba.alaei@gmail.com>
2025-09-19 16:14:21 +0300

    sym

--- INS_SW_MC/Compounds/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-for d in * ; do
-    if [ -d "$d" ]; then
-        # Your operations on directories here
-        echo "Processing directory: $d"
-	cp  ../../../Magnetic_data_add_new_feature/INS_SW_MC/$d/sym.txt   $d/
-    fi
-done
